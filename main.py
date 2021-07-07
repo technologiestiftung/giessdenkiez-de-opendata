173f13ea31665890d59c43ad779a119626869808
Lisa-Stubert <61182572+Lisa-Stubert@users.noreply.github.com>
2021-07-07 12:53:42 +0200

    remove duplicated functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
+import pandas as pd
 from utils.interact_with_db import start_db_connection
-from utils.get_watering_data import read_watering_data, data_to_files
+from utils.get_watering_data import export_watering_data
+from utils.get_KPIs import export_KPIs
 
 # logger configuration
 logger = logging.getLogger('root')
@@ -14,5 +16,17 @@ logger.info("Starting script: GdK Open Data")
 # Connect to the GdK Database, credentials are specified in environment variables
 conn = start_db_connection()
 
-watered_trees_df, watered_trees_gdf = read_watering_data(conn)
-data_to_files(watered_trees_df,watered_trees_gdf)
+# Get watering data from DB and write to files
+sql_query = "SELECT t.id, t.lat, t.lng, t.bezirk, t.artdtsch, t.gattungdeutsch, t.strname, t.pflanzjahr, w.time, w.amount FROM trees_watered AS w JOIN trees_new AS t ON w.tree_id=t.id "
+df_watering = pd.read_sql_query(sql_query, conn)
+
+export_watering_data(df_watering)
+
+# Get KPI data and write to existing file
+sql_query_adopt = "SELECT count(*) AS adoptedTrees, count(DISTINCT tree_id) AS uniqueAdoptedTrees, count(DISTINCT uuid) AS uniqueUsers FROM trees_adopted;"
+df_adopt = pd.read_sql_query(sql_query_adopt, conn)
+
+sql_query_water = "SELECT count(*) AS treesWatered, count(DISTINCT tree_id) AS uniqueTreesWatered, count(DISTINCT uuid) AS uniqueWateringUsers, SUM(amount::INTEGER) AS totalWaterAmount FROM trees_watered;"
+df_water = pd.read_sql_query(sql_query_water, conn)
+
+export_KPIs(df_adopt,df_water)
--- utils/get_KPIs.py
+++ utils/get_KPIs.py
@@ -14,97 +14,23 @@ FORMAT = "[%(levelname)s %(name)s] %(message)s"
 logging.basicConfig(format=FORMAT)
 logger.setLevel(logging.DEBUG)
 
-existing_df = pd.read_csv("daten/giessdenkiezKPIs.csv", sep=",")
 
-
-def start_db_connection():
-    """Loads database parameters from a .env-file and concects to the database.yy
-
-    Raises:
-        Exception: Connecting to the database was not successfull.
-
-    Returns:
-        class 'sqlalchemy.engine.base.Engine': The engine object for connecting to the database.
-    """
-    # load database parameters from .env
-    load_dotenv()
-    # check if all required environmental variables are accessible
-    for env_var in ["PG_DB", "PG_PORT", "PG_USER", "PG_PASS", "PG_SERVER"]:
-        if env_var not in os.environ:
-            logger.error("❌ Environmental Variable {} does not exist".format(env_var))
-    pg_server = os.getenv("PG_SERVER")
-    pg_port = os.getenv("PG_PORT")
-    pg_username = os.getenv("PG_USER")
-    pg_password = os.getenv("PG_PASS")
-    pg_database = os.getenv("PG_DB")
-
-    # create databse connection url from variables
-    conn_string = (
-        "postgresql://"
-        + pg_username
-        + ":"
-        + pg_password
-        + "@"
-        + pg_server
-        + ":"
-        + pg_port
-        + "/"
-        + pg_database
-    )
-    # connect to the database
-    conn = create_engine(conn_string)
-
-    try:
-        conn.connect()
-        logger.info("🗄  Database connection established")
-
-        return conn
-    # stop script if connection to database was not succesfull
-    except:
-        msg = f"❌ Could not establish a database connection to {conn_string}"
-        logger.error(msg)
-        raise Exception(msg)
-
-
-def read_db_data(conn):
-    """Load data about watered trees and information about trees from the database to a dataframe and to a geodataframe.
+def export_KPIs(df_adopt, df_water):
+    """Add KPIs to existing csv file.
 
     Args:
-        conn (class 'sqlalchemy.engine.base.Engine'): The engine object for connecting to the database.
-
-    Returns:
         watered_trees_df: (DataFrame): Watering and tree data that is currently stored in the database.
         watered_trees_gdf: (GeoDataFrame): Watering and tree data that is currently stored in the database with geometry.
     """
+    existing_df = pd.read_csv("daten/giessdenkiezKPIs.csv", sep=",")
 
-    # create query for selecting the data
-    sql_query_adopt = "SELECT count(*) AS adoptedTrees, count(DISTINCT tree_id) AS uniqueAdoptedTrees, count(DISTINCT uuid) AS uniqueUsers FROM trees_adopted;"
-    sql_query_water = "SELECT count(*) AS treesWatered, count(DISTINCT tree_id) AS uniqueTreesWatered, count(DISTINCT uuid) AS uniqueWateringUsers, SUM(amount::INTEGER) AS totalWaterAmount FROM trees_watered;"
-
-    df_adopt = pd.read_sql_query(sql_query_adopt, conn)
-    df_water = pd.read_sql_query(sql_query_water, conn)
-
-    # create dataframe from sql values
     result = df_adopt.join(df_water)
     timestamp = datetime.now().date()
     result["datestamp"] = timestamp
-
-    return result
-
-
-def data_to_files(result):
-    """Write data about watered trees and information about trees to csv and geojson files.
-
-    Args:
-        watered_trees_df: (DataFrame): Watering and tree data that is currently stored in the database.
-        watered_trees_gdf: (GeoDataFrame): Watering and tree data that is currently stored in the database with geometry.
-    """
 
     # open csv in append mode 'a' to add dataframe to existing csv file
     result.to_csv("daten/giessdenkiezKPIs.csv", mode="a", index=False, header=False)
     logger.info("Data was written to csv-file: daten/giessdenkiez_KPIs.csv")
 
 
-conn = start_db_connection()
-result = read_db_data(conn)
-data_to_files(result)
+
